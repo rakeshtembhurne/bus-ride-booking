a87903e977fae353c25cd4594cb2a2c3fd036bb2
Rakesh Tembhurne <rakesh@tembhurne.com>
2025-01-27 22:52:56 +0530

    feat: Remove front page image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,14 @@
-import GoogleFormEmbed from "@/components/sections/google-form";
+// import GoogleFormEmbed from "@/components/sections/google-form";
 import HeroLanding from "@/components/sections/hero-landing";
-import PreviewLanding from "@/components/sections/preview-landing";
+
+// import PreviewLanding from "@/components/sections/preview-landing";
 
 export default function IndexPage() {
   return (
     <>
       {/* <GoogleFormEmbed formUrl="https://docs.google.com/forms/d/e/1FAIpQLSdoQYYugjPKisSpCSTZU4zBftSt3wFzy9Y4U4Zj_JToilyWjQ/viewform?usp=header" /> */}
       <HeroLanding />
-      <PreviewLanding />
+      {/* <PreviewLanding /> */}
     </>
   );
 }
