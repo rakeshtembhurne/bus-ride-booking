8a97760b8e63d2a3f3967dc73235771b70621038
Saifkhan-rkp <saif.pathan@pinnacle.in>
2025-01-27 11:16:08 +0530

    fix: Removed unwanted .history folder

--- .history/lib/validations/fare_20250116163745.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-
-// export const fareSchema = z.object({
-//     routeId: z.string().min(1, "Route ID is required"),
-//     fromLocationId: z.string().min(1, "From Locaiton ID is required"),
-//     toLocationId: z.string().min(1, "To Locaiton ID is required"),
-//     price: z.number().min(0, "Price must be greater than 0"),
-//     createdByUserId: z.string().min(1, "Created By User ID is required"),
-// })
